d90bd2cba7161dce8f90fe9ca9487a18bc53bc5a
keerthanasethu <74970696+keerthanasethu@users.noreply.github.com>
2020-11-25 15:17:40 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,26 @@
+print ("Hello world!")
+print ("30 days 30 hour challenge")
+print ('30 days 30 hour challenge')
+Hours= "thirty"
+print (Hours)
+Days= "Thirty days"
+print (Days[0])
+Challenge = "I Will Win"
+print (Challenge[7:10])
+Challenge = "I Will Win"
+print(len(Challenge))
+Challenge = "I Will Win"
+print(Challenge.lower())
+a= "30 Days"
+b= "30 hours"
+c= a+b
+print (c)
+a= "30 Days"
+b= "30 hours"
+c= a+" "+b
+print (c)
+
+
+String= "Hello Welcome To My World"
+print(String[2:25])
+
