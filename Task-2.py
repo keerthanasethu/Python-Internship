92deb1947768b634df1107c0a766282b4211a07f
keerthanasethu <74970696+keerthanasethu@users.noreply.github.com>
2020-11-25 16:30:02 +0530

    Add files via upload

---
+++ Task-2.py
@@ -0,0 +1,26 @@
+number_of_wheels = 2
+print(number_of_wheels)
+print(24666578+1)
+number = 5
+pie = 3.14
+Name = "Dhoni"
+print number
+print pie
+print Name
+
+a = b = c = 5
+d = a/10
+print d
+e = b*50
+print e
+f = c+60
+print f
+
+String = "python"
+print (String.replace("t", "G"))
+
+a = 5
+b = 3.7
+print (float(a))
+print (int(b))
+
